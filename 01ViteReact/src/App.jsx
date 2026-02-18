a064fe8284f6809bec2c194bfa1ea9cae119ef75
Shivam Mishra <mshivam2062@gmail.com>
2026-02-18 10:50:34 +0530

    Add Server side apporch

--- 01ViteReact/src/App.jsx
+++ 01ViteReact/src/App.jsx
@@ -2,7 +2,13 @@ import Shiv from "./shiv"
 
 function App() {
   return (
+    <>
+    <h1>Hii This is My 3rd Year of B.tech</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente sed enim
+       unde cupiditate consequatur, accusantium officiis distinctio placeat laudantium nisi, 
+      odio harum, incidunt laboriosam. Esse doloribus natus reiciendis aut ullam!</p>
     <Shiv/>
+    </>
     
   );
 }
