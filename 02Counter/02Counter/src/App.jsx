ae5d6cfc90e1d5cbf59bdfe448609aa8a28161fc
Shivam Mishra <mshivam2062@gmail.com>
2026-02-18 15:23:49 +0530

    Add Server side apporch

---
+++ 02Counter/02Counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter , setcounter] = useState(0);
+  // let counter  = 15;
+
+  const addvalue = () => {
+    counter = counter + 1
+    setcounter(counter)
+    console.log(counter)
+  }
+
+
+  const removeValue = () => {
+    // counter = counter-1;
+    setcounter(counter-1);
+    
+  }
+ 
+  return (
+    <>
+      <h1>Hii My Name is Shivam Mishra</h1>
+      <p>Counter Value : {counter}</p>
+
+      <br />
+      <button onClick={addvalue}>Add Value {counter}</button>
+      <br />
+
+      
+      <button onClick={removeValue}>Remove Counter : {counter}</button>
+
+    </>
+  )
+}
+
+export default App
