1302aa4f7c5cf35a4b03a0065d4742463c15d4fd
Shivam Mishra <mshivam2062@gmail.com>
2026-02-19 15:09:39 +0530

    Add Server side apporch

--- 04BackgroundColor/src/App.jsx
+++ 04BackgroundColor/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [color, setColor] = useState("white");
-
+  let [color, setColor] = useState("white");
+  console.log(color);
+  
+  
   return (
     <>
       <div
-        className="w-full min-h-screen transition-all duration-300 flex items-end justify-center"
+        className="transition-all duration-300 flex items-end justify-center"
         style={{ backgroundColor: color }}
       >
         <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
@@ -19,8 +21,30 @@ function App() {
               onClick={() => setColor("red")}
               className="outline-none px-4 py-4 rounded-full text-white shadow-lg"
               style={{ backgroundColor: "red" }}
+              
             >
-              RED
+            RED
+            </button>
+            <button
+              onClick={() => setColor("green")}
+              className="outline-none px-4 py-4 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "green" }}
+            >
+            GREEN
+            </button>
+            <button
+              onClick={() => setColor("yellow")}
+              className="outline-none px-4 py-4 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "yellow" }}
+            >
+            YELLOW
+            </button>
+            <button
+              onClick={() => setColor("pink")}
+              className="outline-none px-4 py-4 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "pink" }}
+            >
+            PINK
             </button>
 
           </div>
