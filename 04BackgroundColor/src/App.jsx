16fb8608b939caad8fe05edf69784a3ec04f2ba7
Shivam Mishra <mshivam2062@gmail.com>
2026-02-19 14:55:23 +0530

    Add Server side apporch

--- 04BackgroundColor/src/App.jsx
+++ 04BackgroundColor/src/App.jsx
@@ -4,30 +4,28 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("white");
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div
+        className="w-full min-h-screen transition-all duration-300 flex items-end justify-center"
+        style={{ backgroundColor: color }}
+      >
+        <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+          <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-3 rounded-3xl">
+
+            <button
+              onClick={() => setColor("red")}
+              className="outline-none px-4 py-4 rounded-full text-white shadow-lg"
+              style={{ backgroundColor: "red" }}
+            >
+              RED
+            </button>
+
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
