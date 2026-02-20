54b88a321f259e6e11be5b78d6d24f9dea0fd1cc
Shivam Mishra <mshivam2062@gmail.com>
2026-02-20 10:21:42 +0530

    Add Server side apporch

--- 04BackgroundColor/src/App.jsx
+++ 04BackgroundColor/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
   
   return (
     <>
+    
       <div
         className="transition-all duration-300 flex items-end justify-center"
         style={{ backgroundColor: color }}
