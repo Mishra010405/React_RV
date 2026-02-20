24ed4acc5147afc04f4bf98ae4c850fb5b10147d
Shivam Mishra <mshivam2062@gmail.com>
2026-02-20 10:36:05 +0530

    Add Server side apporch

--- 05PasswordGenerator/src/App.jsx
+++ 05PasswordGenerator/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState ,  } from 'react'
+import { useCallback, useState ,  } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,11 +6,30 @@ import './App.css'
 function App() {
   const [length, SetLength] = useState(8);
   const [numberAllowed , setNumberAllowed] = useState(false);
-  const [charactor, setCharactoAllowed] = useState(false);
+  const [charallowed, setCharactoAllowed] = useState(false);
   const [password , setPassword] = useState("")
 
 
-  
+  const passwordGenerator  = useCallback(() => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz"
+
+    if(numberAllowed) str += "0123456789"
+    if(charallowed) str += "!@#$%^&*()_+-={}[]|\:;<>,.?/~"
+
+
+    for(let i=1 ; i<=Array.length ; i++) {
+      let char = Math.floor(Math.random()*str.length +1)
+    }
+
+    pass = str.charAt(char)
+
+    setPassword(pass)
+
+  } , [length , charallowed , numberAllowed])
+
+
+
 
 
 
