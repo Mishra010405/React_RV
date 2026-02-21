05894690465c460da6ff5dc78ce5882cf0720cd9
Shivam Mishra <mshivam2062@gmail.com>
2026-02-21 15:54:59 +0530

    Add Server side apporch

--- 05CurrencyConverter/src/hooks/useCurrencyInfo.js
+++ 05CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -5,7 +5,7 @@ function useCurrencyInfo(currency) {
 
   useEffect(() => {
     fetch(
-      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json` // It con on;y convert usd to inr 
+      `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.json` // It con on;y convert usd to inr  Because it use to define the api of only one currency....
     )
       .then((res) => res.json())
       .then((res) => setData(res[currency]))
