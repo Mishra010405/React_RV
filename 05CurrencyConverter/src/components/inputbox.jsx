95bec33cc8b17c1a707ae25e5bbcc3246dfbe3d4
Shivam Mishra <mshivam2062@gmail.com>
2026-02-21 15:11:00 +0530

    Add Server side apporch

---
+++ 05CurrencyConverter/src/components/inputbox.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Inputbox() {
+    label,
+    amount,
+    onamountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+    amountdisable = false,
+    currentdisable = false,
+    className = "",
+}
