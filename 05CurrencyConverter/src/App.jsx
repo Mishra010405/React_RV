5f79b641db5b37d1907191b8d57c50c747b4394a
Shivam Mishra <mshivam2062@gmail.com>
2026-02-21 15:34:22 +0530

    Add Server side apporch

--- 05CurrencyConverter/src/App.jsx
+++ 05CurrencyConverter/src/App.jsx
@@ -29,5 +29,72 @@ function App() {
     setConvertedAmount(amount*currencyInfo[to])
   }
 
-  
+  return (
+    <>
+      <div
+        className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
+        style={{
+          backgroundImage: `url('https://images.pexels.com/photos/3483098/pexels-photo-3483098.jpeg')`,
+        }}
+      >
+        <div className="w-full">
+          <div className="w-full max-w-md mx-auto bg-yellow-200 p-6 rounded-lg shadow-lg">
+            <form
+              onSubmit={(e) => {
+                e.preventDefault()
+                convert()
+              }}
+            >
+              {/* From Input */}
+              <div className="w-full mb-1">
+                <InputBox
+                  label="From"
+                  amount={amount}
+                  currencyOptions={options}
+                  onAmountChange={(value) => setAmount(value)}
+                  onCurrencyChange={(currency) => setFrom(currency)}
+                  selectCurrency={from}
+                />
+              </div>
+
+              {/* Middle Button (Swap) */}
+              <div className="relative w-full h-0.5 my-3">
+                <button
+                  
+                  type="button"
+                  onClick={swap}
+                  className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-3 py-1"
+                >
+                  Swap
+              
+                </button>
+              </div>
+
+              {/* To Input */}
+              <div className="w-full mt-1 mb-4">
+                <InputBox
+                  label="To"
+                  amount={convertedAmount}
+                  currencyOptions={options}
+                  onCurrencyChange={(currency) => setTo(currency)}
+                  selectCurrency={to}
+                  amountDisabled
+                />
+              </div>
+
+              {/* Submit Button */}
+              <button
+                type="submit"
+                className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg"
+              >
+                Convert
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
 export default App
