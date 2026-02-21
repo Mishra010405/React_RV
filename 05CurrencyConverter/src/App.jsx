b18c7bbb7349705616eafd6acdd9a3b1a6e72ed9
Shivam Mishra <mshivam2062@gmail.com>
2026-02-21 15:34:13 +0530

    Add Server side apporch

--- 05CurrencyConverter/src/App.jsx
+++ 05CurrencyConverter/src/App.jsx
@@ -2,15 +2,32 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import useCurrencyInfo from './hooks/useCurrencyInfo'
+
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount , setAmount] = useState(0);
+  const [from , setFrom] = useState("usd");
+  const [to,setTo]  = useState("inr")
+  const [convertedAmount  , setConvertedAmount] = useState(0);
+  const currencyInfo = useCurrencyInfo(from)
 
-  return (
-    <>
-    <h1>Hii My Name is Shivam Mishra</h1>
-    </>
-  )
-}
+  const options = Object.keys(currencyInfo)
 
+  const swap = () => {
+    setFrom(to);
+    setTo(from)
+    setAmount(convertedAmount)
+    setConvertedAmount(amount)
+
+  }
+
+  const convert = () => {
+    setConvertedAmount(amount*currencyInfo[to])
+  }
+
+  
 export default App
