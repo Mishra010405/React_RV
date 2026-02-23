80ffaefa98d084b2bb62252fed53acea5080c808
Shivam Mishra <mshivam2062@gmail.com>
2026-02-23 19:35:31 +0530

    Add Server side apporch

--- 07reactrouter/src/App.jsx
+++ 07reactrouter/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './Components/header/header'
+import Home from './Components/Home/home'
+import Footer from './Components/footer/footer'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-    <h1>Hii My Name is Shivam Mishta</h1>
-    </>
+   
+    
+    
   )
 }
 
