e3a6ae6a82193029e1db51282d47b5640646376f
Shivam Mishra <mshivam2062@gmail.com>
2026-02-23 19:44:00 +0530

    Add Server side apporch

--- 07reactrouter/src/App.jsx
+++ 07reactrouter/src/App.jsx
@@ -2,9 +2,6 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Header from './Components/header/header'
-import Home from './Components/Home/home'
-import Footer from './Components/footer/footer'
 
 
 function App() {
