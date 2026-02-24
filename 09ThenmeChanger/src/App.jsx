f05c809e6c1ea3b76f1a6cce68f1d89ed87c7bc0
Shivam Mishra <mshivam2062@gmail.com>
2026-02-24 15:45:45 +0530

    Add Server side apporch

--- 09ThenmeChanger/src/App.jsx
+++ 09ThenmeChanger/src/App.jsx
@@ -1,9 +1,9 @@
 
 import { useEffect, useState } from 'react'
 import './App.css'
-import { ThemeProvider } from './contexts/theme'
+import { ThemeProvider } from './Context/theme'
 import ThemeBtn from './Components/Themebtn'
-import Card from './components/Card'
+import Card from './Components/Cards'
 
 function App() {
   const [themeMode, setThemeMode] = useState("light")
--- 09ThenmeChanger/src/Components/Themebtn.jsx
+++ 09ThenmeChanger/src/Components/Themebtn.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import useTheme from '../contexts/theme';
+import useTheme from '../Context/theme';
 
 export default function ThemeBtn() {
     
