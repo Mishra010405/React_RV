d5e94f862afaaacd472aa532375475e8a2dcdca8
Shivam Mishra <mshivam2062@gmail.com>
2026-02-24 15:48:41 +0530

    Add Server side apporch

--- 09ThenmeChanger/src/Components/Cards.jsx
+++ 09ThenmeChanger/src/Components/Cards.jsx
@@ -9,7 +9,7 @@ export default function Card() {
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
-                        Watch Virat Kohli play his best innings.
+                        Watch Virat Kohli play his best innings. In to the World Cup.
                     </h5>
                 </a>
                 <div className="flex items-center mt-2.5 mb-5">
