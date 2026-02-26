d93e190f2c9eaed9a861519e981e4c5b6cd09d61
Shivam Mishra <mshivam2062@gmail.com>
2026-02-26 12:08:00 +0530

    React- Redux

--- 11ReactRedux/src/features/todos/todoSlice.js
+++ 11ReactRedux/src/features/todos/todoSlice.js
@@ -1,4 +1,5 @@
 import { createSlice , nanoid} from "@reduxjs/toolkit";
+import { act } from "react";
 // import { act} from "react";
 
 const initialState = {
@@ -10,6 +11,7 @@ export const todoSlice = createSlice({
     name :'todo',
     initialState,
     reducers: {
+        // Add Todo
         addTodo: (state, action) => {
             const todo = {
                 id: nanoid(),
@@ -17,14 +19,26 @@ export const todoSlice = createSlice({
             }
             state.todos.push(todo)
         },
-
+        // removeTodo
         removeTodo : (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !=action.payload)
-        }
+        },
+        
+        // Update Todo
+        updateTodo: (state, action ) => {
+            const {id,text} = action.payload;
 
+            const todo = state.todos.find(
+                (todo) => todo.id === id
+            );
+            if(todo) {
+                todo.text = text;   
+            }
+
+        }
     }
 })
 
-export const {addTodo , removeTodo} = todoSlice.actions
+export const {addTodo , removeTodo, updateTodo} = todoSlice.actions
 
 export default todoSlice.reducer
