cabf8429ce33a655718a072593b4dbb60bec99dd
Shivam Mishra <mshivam2062@gmail.com>
2026-02-27 12:29:50 +0530

    React- Redux

--- 11ReactRedux/src/App.jsx
+++ 11ReactRedux/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Todos from './Components/Todos'
 
 function App() {
   
@@ -9,6 +10,8 @@ function App() {
   return (
     <>
     <h1>Hii, Mty Name is Shivam Mishra</h1>
+    <addTodo/>
+    <Todos/>
     </>
     
   )
---
+++ 11ReactRedux/src/Components/Todos.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { removeTodo } from "../features/todos/todoSlice";
+
+function Todos() {
+
+  const todos = useSelector(state => state.todos.todos);
+  const dispatch = useDispatch();
+
+  return (
+    <>
+      <div className="text-white text-2xl">Todos</div>
+
+      <ul className="list-none">
+        {todos.map((todo) => (
+          <li
+            className="mt-4 flex justify-between items-center bg-zinc-800 px-4 py-2 rounded"
+            key={todo.id}
+          >
+            <div className="text-white">{todo.text}</div>
+
+            <button
+              onClick={() => dispatch(removeTodo(todo.id))}
+              className="text-white bg-red-500 border-0 py-1 px-4 focus:outline-none hover:bg-red-600 rounded text-md"
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default Todos;
