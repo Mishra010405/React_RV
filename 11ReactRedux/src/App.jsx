78fc538b6b19e333ae6dce3cf1380302e8007e20
Shivam Mishra <mshivam2062@gmail.com>
2026-03-01 12:27:42 +0530

    React- Redux

--- 11ReactRedux/src/App.jsx
+++ 11ReactRedux/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-    <h1>Hii, My Name is Shivam Mishra , This is React project by me </h1>
+    <h1>Hii, My Name is Shivam Mishra  </h1>
     <AddTodo/>
     <Todos/>
     </>
