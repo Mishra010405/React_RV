931f9d2d34c402fd06148f20633b3e4495e2fc8a
Shivam Mishra <mshivam2062@gmail.com>
2026-03-03 15:40:48 +0530

    React- Redux

--- 12Apwrite/src/main.jsx
+++ 12Apwrite/src/main.jsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import store from './Store/Store.js'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <React.StrictMode>
+    < Provider store={store}>
+    <RouterProvider router={router}/>
     <App />
-  </StrictMode>,
+    </Provider>
+  </React.StrictMode>,
 )
