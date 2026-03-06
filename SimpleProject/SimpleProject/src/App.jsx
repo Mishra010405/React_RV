57651d36ff7aebbd436f73a9e22b98010d2766e2
Shivam Mishra <mshivam2062@gmail.com>
2026-03-06 10:34:07 +0530

    React- Redux

---
+++ SimpleProject/SimpleProject/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [value, setValue] = useState(1);
+  const [multiplied, setMultiplied] = useState(1);
+
+  const multipliedfive = () => {
+    setMultiplied(value * 5);
+    setValue(value+1);
+  };
+
+  return (
+    <>
+      <h1>Main Value: {value}</h1>
+
+      <button onClick={multipliedfive}>
+        Click to Multiply
+      </button>
+
+      <hr />
+
+      <h2>Multiplied Value: {multiplied}</h2>
+    </>
+  );
+}
+
+export default App;
