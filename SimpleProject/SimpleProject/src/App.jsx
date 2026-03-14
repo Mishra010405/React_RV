7ff4bf9a70403739245b888b88f69e11e94f5a8a
Shivam Mishra <mshivam2062@gmail.com>
2026-03-14 20:09:26 +0530

    Simple-Project

--- SimpleProject/SimpleProject/src/App.jsx
+++ SimpleProject/SimpleProject/src/App.jsx
@@ -12,11 +12,7 @@ function App() {
 
   return (
     <>
-      <h1>Main Value: {value}</h1>
-
-      <button onClick={multipliedfive}>
-        Click to Multiply
-      </button>
+      
 
       <hr />
 
