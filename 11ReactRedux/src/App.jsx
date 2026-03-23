f96d0961b2785f1b3eee27ccfac9136b02773f0d
Shivam Mishra <mshivam2062@gmail.com>
2026-03-23 18:26:58 +0530

    TODO With TypeScript

--- 11ReactRedux/src/App.jsx
+++ 11ReactRedux/src/App.jsx
@@ -7,9 +7,7 @@ import AddTodo from './Components/AddTodo'
 
 function App() {
     return (
-      <>
-      <h1>Learn About the redux toolkit</h1>
-      <AddTodo/>
+      <>/>
       <Todos/>
       </>
     )
